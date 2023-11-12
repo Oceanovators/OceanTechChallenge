582562b51be5dfc1b09a838248fdfae31599a104
sijan67 <sijuichi1@gmail.com>
2023-11-12 00:22:31 -0800

    improved visualization for drone and landfill page

--- waste_dashboard.py
+++ waste_dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import random
+import numpy as np
 
 # Mock data for Trash Bins
 trash_bin_data = pd.DataFrame({
@@ -15,15 +16,36 @@ trash_bin_data = pd.DataFrame({
 drone_data = pd.DataFrame({
     'Date': pd.date_range(start='2023-01-01', periods=5, freq='D'),
     'Waste_Type': ['Plastic', 'Metal', 'Paper', 'Plastic', 'Glass'],
-    'Quantity': [50, 30, 20, 40, 25]
+    'Quantity': [random.randint(10, 50) for _ in range(5)]  # Mock quantities for each waste type
 })
+
+# Expand drone data to include quantities for all waste categories
+all_categories = ['Cardboard', 'Glass', 'Metal', 'Paper', 'Plastic', 'Compost', 'Trash']
+expanded_drone_data = pd.DataFrame(columns=['Date', 'Waste_Type', 'Quantity'])
+for date in drone_data['Date']:
+    for category in all_categories:
+        if category == 'Plastic':
+            quantity = int(np.random.normal(30, 5))  # Generating normally distributed quantities for plastic
+        else:
+            quantity = random.randint(5, 20)
+        expanded_drone_data = expanded_drone_data.append({'Date': date, 'Waste_Type': category, 'Quantity': max(quantity, 0)}, ignore_index=True)
 
 # Mock data for Landfills/Beaches
 landfill_data = pd.DataFrame({
     'Date': pd.date_range(start='2023-01-01', periods=5, freq='D'),
     'Waste_Type': ['Plastic', 'Metal', 'Paper', 'Plastic', 'Glass'],
-    'Quantity': [100, 80, 60, 120, 90]
+    'Quantity': [random.randint(50, 100) for _ in range(5)]  # Mock quantities for each waste type
 })
+
+# Expand landfill data to include quantities for all waste categories
+expanded_landfill_data = pd.DataFrame(columns=['Date', 'Waste_Type', 'Quantity'])
+for date in landfill_data['Date']:
+    for category in all_categories:
+        if category == 'Plastic':
+            quantity = int(np.random.normal(70, 10))  # Generating normally distributed quantities for plastic
+        else:
+            quantity = random.randint(30, 80)
+        expanded_landfill_data = expanded_landfill_data.append({'Date': date, 'Waste_Type': category, 'Quantity': max(quantity, 0)}, ignore_index=True)
 
 # Streamlit App
 st.title("Waste Surveillance Dashboard")
@@ -39,9 +61,9 @@ if selected_page == "Trash Bin":
 # Drone Section
 elif selected_page == "Drone":
     st.header("Drone Data")
-    st.plotly_chart(px.scatter(drone_data, x='Date', y='Quantity', color='Waste_Type', title='Drone Data'))
+    st.plotly_chart(px.scatter(expanded_drone_data, x='Date', y='Quantity', color='Waste_Type', title='Drone Data'))
 
 # Landfills/Beaches Section
 elif selected_page == "Landfill":
     st.header("Landfills/Beaches Monitoring")
-    st.plotly_chart(px.bar(landfill_data, x='Date', y='Quantity', color='Waste_Type', title='Landfills/Beaches Data'))
+    st.plotly_chart(px.bar(expanded_landfill_data, x='Date', y='Quantity', color='Waste_Type', title='Landfills/Beaches Data'))
